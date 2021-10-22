4e5cdcd8d643cd89eaf01dc6daf4cc8896e71dac
HyangRim <ccamang@COBBYui-MacBookAir.local>
2021-10-23 00:41:15 +0900

    돔을 이용하여 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const id  = document.querySelector('#id');
+const pwd  = document.querySelector('#pwd');
+const loginbtn = document.querySelector('button');
+
+loginbtn.addEventListener("click", login)
+
+function login(){
+  
+  const req = {
+    id : id.value,
+    pwd : pwd.value
+  }
+console.log(req);
+}
